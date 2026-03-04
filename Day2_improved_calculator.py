4ce6ee723a14a86ee795c2c22415d119347a109f
luannguyen-ops <luannguyenhaha7@gmail.com>
2026-03-04 20:33:18 +0700

    Implement basic calculator with user input
    Added a basic calculator function that performs addition, subtraction, multiplication, and division. Included input handling for user interaction.

--- Day2_improved_calculator.py
+++ Day2_improved_calculator.py
@@ -1,1 +1,29 @@
+import sys
+def calculator(a, b, phep_tinh):
+    if(phep_tinh == '+'):
+        print(f'{a} + {b} = {a + b}')
+    elif(phep_tinh == '-'):
+        print(f'{a} - {b} = {a - b}')
+    elif(phep_tinh == '*'):
+        print(f'{a} * {b} = {a * b}')
+    elif(phep_tinh == '/'):
+        if(b == 0):
+            print('Mau so khong the bang 0')
+        else:
+            print(f'{a} / {b} = {a / b}')
+
+print("Nhap gia tri va phep tinh")
+
+while True:
+    try:
+        a = int(input(">>>"))
+        phep_tinh = input(">>>")
+        if phep_tinh not in ['+', '-', '*','/']:
+            print('Nhap sai phep tinh! Xin hay nhap lai!!!')
+            continue
+        b = int(input(">>>"))
+        calculator(a, b, phep_tinh)
+        break
+    except ValueError:
+        print('Hay nhap lai')
 
